6d4a1b7fa3bc1f705b9d16a915bb718698291298
Marcel Loose <loose@astron.nl>
2024-06-17 23:29:21 +0200

    Add possibility to set target CPU architecture
    This PR adds the possibility to specify for which CPU architecture the compiler should build binaries. It replaces the existing method where one or more AVX extensions could be enabled by setting the associated variables. The rationale is that it makes more sense to optimize for a certain CPU micro-architecture (and its successors) than to only enable specific AVX features.
    
    The `PORTABLE` option is retained and its behavior doesn't change. I.e., setting `PORTABLE=ON` will instruct the compiler to build binaries that can be run on any X86-64 CPU; setting `PORTABLE=OFF` will instruct the compiler to use the native CPU as target. The user can instruct the compiler to target a specific CPU by setting the CMake variable `TARGET_CPU`. Setting `TARGET_CPU` if `PORTABLE=ON` will raise an error. Setting `TARGET_CPU` to a CPU type the compiler doesn't support will also raise an error.

---
+++ CMake/SetTargetCPU.cmake
@@ -0,0 +1,36 @@
+# Instruct the compiler to generate portable binaries if `PORTABLE=ON`.
+# If `PORTABLE=OFF`, instruct the compiler to generate binaries that are optimized
+# for the native CPU. The user may override this by setting `TARGET_CPU` to a
+# specifc CPU architecture, e.g. `TARGET_CPU=haswell`. If the compiler doesn't know
+# how to generate code for the given `TARGET_CPU`, an error will be raised.
+# You cannot set `TARGET_CPU` if `PORTABLE=ON`.
+
+include(CheckCXXCompilerFlag)
+if(PORTABLE)
+  if(DEFINED TARGET_CPU)
+    message(
+      FATAL_ERROR
+        "You cannot specify a target CPU when building portable binaries. "
+        "Set PORTABLE=OFF if you want to set TARGET_CPU.")
+  endif()
+  message(
+    WARNING
+      "Building portable binaries, which will have slighly decreased performance."
+  )
+else()
+  if(NOT DEFINED TARGET_CPU)
+    set(TARGET_CPU native)
+  endif()
+  # Unset cached variable to force a check; the user may have given a different
+  # value for TARGET_CPU.
+  unset(COMPILER_SUPPORTS_TARGET_CPU CACHE)
+  check_cxx_compiler_flag("-march=${TARGET_CPU}" COMPILER_SUPPORTS_TARGET_CPU)
+  if(COMPILER_SUPPORTS_TARGET_CPU)
+    add_compile_options(-march=${TARGET_CPU})
+  else()
+    message(
+      FATAL_ERROR
+        "The compiler doesn't support '${TARGET_CPU}' as target CPU architecture."
+    )
+  endif()
+endif(PORTABLE)
